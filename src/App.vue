b2e33f34a519db1c85563ba6c0e651ab91dbdf2f
xrz-cloud <62951481+xrz-cloud@users.noreply.github.com>
2022-12-04 15:13:00 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-config-provider :size="size" :z-index="zIndex" :locale="locale">
+    <RouterLink to="/"></RouterLink>
+    <RouterView />
+  </el-config-provider>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { ElConfigProvider } from "element-plus";
+
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+import { RouterView } from "vue-router";
+
+export default defineComponent({
+  components: {
+    ElConfigProvider,
+    RouterView,
+  },
+  setup() {
+    return {
+      zIndex: 3000,
+      size: "small",
+      locale: zhCn,
+    };
+  },
+});
+</script>
