b1aef19d54b4df2dec39a164f5a79d77dfc5e848
xrz-cloud <3545270592@qq.com>
2022-12-25 15:48:17 +0800

    能用

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <el-config-provider :size="size" :z-index="zIndex" :locale="locale">
-    <RouterLink to="/"></RouterLink>
     <RouterView />
   </el-config-provider>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
 import { ElConfigProvider } from "element-plus";
 
-import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+import zhCn from "./element-plus-locale-zh-cn.mjs";
 import { RouterView } from "vue-router";
 
 export default defineComponent({
@@ -20,7 +19,7 @@ export default defineComponent({
   setup() {
     return {
       zIndex: 3000,
-      size: "small",
+      size: "default",
       locale: zhCn,
     };
   },
---
+++ src/views/DlView.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { useQuery } from "@urql/vue";
+import router from "../router";
+import { ref } from "vue";
+
+const $$route = router.currentRoute.value;
+let result;
+const dl = ref("awesome");
+
+async function main() {
+  result = await useQuery({
+    query: `
+  query($path: String!) {
+    od(path: $path) {
+      raw {
+        dlinks {
+          dlink,
+          sharelink
+        }
+      }
+    }
+  }
+`,
+    variables: {
+      path: "/index/md/" + $$route.params.mdid + "/" + $$route.params.fn,
+    },
+  });
+
+  dl.value = await result.data.value.od.raw.dlinks[0].dlink;
+
+  console.log(dl.value);
+}
+main();
+</script>
+
+<template>
+  <main>
+    <p>番剧:md{{ $route.params.mdid }}<br />文件名:{{ $route.params.fn }}</p>
+    <a :href="dl">下载</a>
+  </main>
+</template>
